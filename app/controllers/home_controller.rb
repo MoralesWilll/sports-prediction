984401da6e8aabd14beac6d41f1f2cff4a1861a0
Daws <castrogiraldosantiago@gmail.com>
2025-01-27 22:27:57 -0500

    Feat: basic meta tags configurations on root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    set_meta_tags(
+      title: 'Home page',
+      description: 'AI prediction for champions league soccer matches, do a better prediction whit our trained model',
+      keywords: 'partidos, fútbol, estadísticas,Soccer, champions, prediction, stadistics ',
+      follow: true
+    )
   end
 end
